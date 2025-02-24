cfe1b570f51d7c34e17e092b941acd922511ebce
Lunara-Jered <nemesislunaken@gmail.com>
2025-02-24 12:16:54 +0100

    Update feedscreen.dart

--- lib/screens/feedscreen.dart
+++ lib/screens/feedscreen.dart
@@ -90,7 +90,7 @@ class _FeedScreenState extends State<FeedScreen> {
       appBar: AppBar(
         title: const Text("Actualités Politiques", style: TextStyle(color: Colors.white, fontSize: 18)),
         backgroundColor: Colors.blue,
-      ), elevation: 0,
+      ),
         actions: [
           IconButton(
             onPressed: () {
